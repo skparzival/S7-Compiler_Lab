4dd1a30add5968dbd7c731ce63f68607707c7c39
suryakiran <skparzival264@gmail.com>
2023-12-02 19:45:07 +0530

    updated enfa to nfa

--- ENfA-to-NFA/etn.c
+++ ENfA-to-NFA/etn.c
@@ -18,17 +18,16 @@ static int e_closure[20][20]={0};
 struct node * transition[20][20]={NULL};
 void main()
 {
-          int i,j,k,m,t,n;
-          struct node *temp;
-          printf("Enter the no. of alphabets: ");
-          scanf("%d",&noalpha);
-          getchar();
-          printf("NOTE:- [ use letter e as epsilon]\n");
-          printf("NOTE:- [e must be last character ,if it is present]\n");
-          printf("\nEnter alphabets: \n");
-          for(i=0;i<noalpha;i++)
-         {
-
+        int i,j,k,m,t,n;
+        struct node *temp;
+        printf("Enter the no. of inputs: ");
+        scanf("%d",&noalpha);
+        getchar();
+        printf("NOTE:-[use letter e as epsilon]\n");
+        printf("NOTE:-[e must be last character, if it is present]\n");
+        printf("\nEnter inputs: ");
+        for(i=0;i<noalpha;i++)
+        {
                   alphabet[i]=getchar();
                   getchar();
         }
@@ -41,9 +40,9 @@ void main()
         printf("Enter the final states: \n");
         for(i=0;i<nofinal;i++)
                 scanf("%d",&finalstate[i]);
-         printf("Enter no. of transition: ");
+        printf("Enter no. of transition: ");
         scanf("%d",&notransition);
-        printf("\nEnter transitions: \n");
+        printf("Enter transitions: \n");
         for(i=0;i<notransition;i++)
         {
                 scanf("%d %c%d",&r,&c,&s);
@@ -55,8 +54,8 @@ void main()
                 c=0;
                 for(j=0;j<20;j++)
                 {
-                              buffer[j]=0;
-                               e_closure[i][j]=0;
+                        buffer[j]=0;
+                        e_closure[i][j]=0;
                 }
                 findclosure(i,i);
         }
@@ -68,115 +67,114 @@ void main()
         printf("\nStates :" );
         for(i=1;i<=nostate;i++)
                   print_e_closure(i);
-
         printf("\nTransitions: \n");
-
         for(i=1;i<=nostate;i++)
         {
-                  for(j=0;j<noalpha-1;j++)
-                 {
-                          for(m=1;m<=nostate;m++)
-                                        set[m]=0;
-                          for(k=0;e_closure[i][k]!=0;k++)
-                          {
-                                    t=e_closure[i][k];
-                                   temp=transition[t][j];
-                                   while(temp!=NULL)
-                                  {
-                                             unionclosure(temp->st);
-                                            temp=temp->link;
-                                   }
-                         }
+                for(j=0;j<noalpha-1;j++)
+                {
+                        for(m=1;m<=nostate;m++)
+                                set[m]=0;
+                        for(k=0;e_closure[i][k]!=0;k++)
+                        {
+                                t=e_closure[i][k];
+                                temp=transition[t][j];
+                                while(temp!=NULL)
+                                {
+                                        unionclosure(temp->st);
+                                        temp=temp->link;
+                                }
+                        }
                         printf("\n");
                         print_e_closure(i);
                         printf("%c\t",alphabet[j]);
                         printf("{");
                         for(n=1;n<=nostate;n++)
                         {
-                                     if(set[n]!=0)
-                                             printf("q%d,",n);
+                                if(set[n]!=0)
+                                        printf("q%d,",n);
                         }
-                         printf("}");
+                        printf("}");
                 }
         }
         printf("\nFinal states:");
         findfinalstate();
+        printf("\n");
 }
 
 void findclosure(int x,int sta)
 {
-            struct node *temp;
-            int i;
-           if(buffer[x])
-                     return;
-             e_closure[sta][c++]=x;
-            buffer[x]=1;
-             if(alphabet[noalpha-1]=='e' && transition[x][noalpha-1]!=NULL)
+        struct node *temp;
+        int i;
+        if(buffer[x])
+                return;
+        e_closure[sta][c++]=x;
+        buffer[x]=1;
+        if(alphabet[noalpha-1]=='e' && transition[x][noalpha-1]!=NULL)
+        {
+                temp=transition[x][noalpha-1];
+                while(temp!=NULL)
                 {
-                             temp=transition[x][noalpha-1];
-                             while(temp!=NULL)
-                            {
-                                         findclosure(temp->st,sta);
-                                         temp=temp->link;
-                             }
+                        findclosure(temp->st,sta);
+                        temp=temp->link;
                 }
-  }
+        }
+}
 
 void insert_trantbl(int r,char c,int s)
 {
-           int j;
-           struct node *temp;
-            j=findalpha(c);
-          if(j==999)
-          {
-                     printf("error\n");
-                    exit(0);
-          }
-         temp=(struct node *) malloc(sizeof(struct node));
-         temp->st=s;
-         temp->link=transition[r][j];
-         transition[r][j]=temp;
+        int j;
+        struct node *temp;
+        j=findalpha(c);
+        if(j==999)
+        {
+                printf("error\n");
+                exit(0);
+        }
+        temp=(struct node *) malloc(sizeof(struct node));
+        temp->st=s;
+        temp->link=transition[r][j];
+        transition[r][j]=temp;
 }
 
 int findalpha(char c)
 {
-            int i;
-            for(i=0;i<noalpha;i++)
-                   if(alphabet[i]==c)
-                          return i;
+        int i;
+        for(i=0;i<noalpha;i++)
+                if(alphabet[i]==c)
+                        return i;
 
-                return(999);
+        return(999);
 
 
 }
 
 void unionclosure(int i)
 {
-              int j=0,k;
-             while(e_closure[i][j]!=0)
-             {
-                      k=e_closure[i][j];
-                      set[k]=1;
-                      j++;
-             }
+        int j=0,k;
+        while(e_closure[i][j]!=0)
+        {
+                k=e_closure[i][j];
+                set[k]=1;
+                j++;
+        }
 }
 void findfinalstate()
 {
-            int i,j,k,t;
-            for(i=0;i<nofinal;i++)
-           {
-                      for(j=1;j<=nostate;j++)
-                      {
-                              for(k=0;e_closure[j][k]!=0;k++)
+        int i,j,k,t;
+        for(i=0;i<nofinal;i++)
+        {
+                for(j=1;j<=nostate;j++)
+                {
+                        for(k=0;e_closure[j][k]!=0;k++)
+                        {
+                                        if(e_closure[j][k]==finalstate[i])
                                 {
-                                         if(e_closure[j][k]==finalstate[i])
-                                        {
 
-                                                 print_e_closure(j);
-                                        }
-                               }
-                      }
-             }
+                                                print_e_closure(j);
+                                }
+                        }
+                }
+        }
 
 
   }
@@ -185,7 +183,7 @@ void print_e_closure(int i)
 {
         int j=0;
         printf("{");
-       if(e_closure[i][j]!=0)
-                        printf("q%d,",e_closure[i][0]);
-         printf("} ");
+        if(e_closure[i][j]!=0)
+                printf("q%d,",e_closure[i][0]);
+        printf("} ");
 }
